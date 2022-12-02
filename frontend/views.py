034d30889c90b0d46a0d014fe10949886f415798
Ik-bruno <apple@APPLEs-MacBook-Pro.local>
2022-12-02 08:11:22 +0100

    first commit

---
+++ frontend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+# Create your views here.
+def home(request):
+        return render(request, 'frontend/frontend.html')
