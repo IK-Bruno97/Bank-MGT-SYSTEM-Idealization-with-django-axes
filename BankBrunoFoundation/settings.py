8ca1d98094a70bec609e9381cbef3b0312ec1add
IK-Bruno97 <69594989+IK-Bruno97@users.noreply.github.com>
2022-12-02 08:57:16 +0100

    Update settings.py

--- BankBrunoFoundation/settings.py
+++ BankBrunoFoundation/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-3snwi3c=)*#1g4u*bwq@cl4l=%#-8l^w6-z^dqa!=p1np9lx2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
